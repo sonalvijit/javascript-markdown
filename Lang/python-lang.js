a47f4f3224b0d2fc3b52108e44a98626a9f30f89
svjt <josephtoppo.sxd@gmail.com>
2025-06-16 00:06:20 +0530

    Added Colour pallete

--- Lang/python-lang.js
+++ Lang/python-lang.js
@@ -1,4 +1,9 @@
-const keyword = ["from","import","return","if","else","def","__name__"]
+const keyword = ['False', 'True', 'None', 'and', 'or', 'not', 'if', 'elif', 'else', 'while', 'for', 'break', 'continue', 'def', 'return', 'lambda', 'yield', 'import', 'from', 'as', 'class', 'try', 'except', 'finally', 'raise', 'assert', 'with', 'pass', 'global', 'nonlocal', 'del', 'async', 'await', 'is', 'in'] // #c586bb
+
+const operators = ["and", "or", "not", "is", "is not", "in", "not in"] // #569ece
+const decorator = ["@"] // #e0d0ac
+const purple_op = ["True", "False", "None", "self", "cls", "__name__", "__main__"] // #886ce3
+const python_function = "#e0d0ac";// #e0d0ac
 
 iteratethrough = (code_string) => {
      // code_string.forEach((line, index) => {
