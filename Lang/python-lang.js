28b557acc4276ff4bd299a1181a01dfe1687265d
svjt <josephtoppo.sxd@gmail.com>
2025-06-16 00:31:15 +0530

    Parsing colour to code through iteratethrough()

--- Lang/python-lang.js
+++ Lang/python-lang.js
@@ -2,7 +2,7 @@ const keyword = ['False', 'True', 'None', 'and', 'or', 'not', 'if', 'elif', 'els
 
 const operators = ["and", "or", "not", "is", "is not", "in", "not in"] // #569ece
 const decorator = ["@"] // #e0d0ac
-const purple_op = ["True", "False", "None", "self", "cls", "__name__", "__main__"] // #886ce3
+const purple_op = ["self", "cls", "__name__", "__main__"] // #886ce3
 const python_function = "#e0d0ac";// #e0d0ac
 
 iteratethrough = (code_string) => {
@@ -10,14 +10,38 @@ iteratethrough = (code_string) => {
      //      console.log(`${index}: ${line}`);
      // });
 
+     // code_string.forEach((line, index)=>{
+     //      const split_ = line.split(" ");
+     //      // for (const line of split_){
+     //      //      if (keyword.includes(line)){
+     //      //           console.log(`${line}: keyword`);
+     //      //      }
+     //      // }
+     //      // console.log(split_);
+     //      console.log(line);
+     // });
+
+
      code_string.forEach((line, index)=>{
           const split_ = line.split(" ");
+          let output = "";
           for (const line of split_){
                if (keyword.includes(line)){
-                    console.log(`${line}: keyword`);
+                    output += `<span style="color:#c586bb">${line}</span> `;
                }
-          }
-          console.log(split_);
+               else if (operators.includes(line)){
+                    output += `<span style="color:#569ece">${line}</span> `;
+               }
+               else if (decorator.includes(line)){
+                    output += `<span style="color:#e0d0ac">${line}</span> `;
+               }
+               else if (purple_op.includes(line)){
+                    output += `<span style="color:#886ce3">${line}</span> `;
+               } else {
+                    output += `<span stype="color:#4bcaaf">${line}</span> `;
+               }
+          };
+          console.log(`${output}<br>`);
      });
 };
 
---
+++ clean.py
@@ -0,0 +1,5 @@
+lst:str="""False,True,None,and,or,not,if,elif,else,while,for,break,continue,def,return,lambda,yield,import,from,as,class,try,except,finally,raise,assert,with,pass,global,nonlocal,del,async,await,is,in"""
+
+a=lst.split(",")
+print(len(a))
+print(a)
